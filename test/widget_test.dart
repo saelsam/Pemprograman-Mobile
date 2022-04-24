6154a41c8cff6f3fb0dcca19e0a18febbbb350d8
saelsam <saelsamuelrude@gmail.com>
2022-04-24 23:45:41 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cobake3/main.dart';
+import 'package:postest4_2009106034_saelsamuelrudek/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
